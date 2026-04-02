74491495d5cd2cf2ea8efe2e7adfe15e16ec1752
Martin F Duran <fduran.net@gmail.com>
2026-04-01 21:17:52 -0400

    Add files via upload

--- core/supabase.js
+++ core/supabase.js
@@ -1,10 +1,3 @@
 // core/supabase.js
 
-import { createClient } from "https://esm.sh/@supabase/supabase-js";
-
-// 🔐 CONFIGURA ESTO CON TUS DATOS REALES
-const SUPABASE_URL = "https://mugufzwvwteoopjdrheq.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_npn2NhS9fEHAjWHGsbLTSQ_KudyqrFt";
-
-// 🚀 CLIENTE GLOBAL
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export { supabase } from "../auth.js";
